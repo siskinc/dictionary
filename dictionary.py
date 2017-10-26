9f3f4b07f3e2faeccc02e747d10ec3ec6eabc0f9
susecjh <susecjh@gmail.com>
2017-10-26 22:02:02 +0800

    有道,普通的查询没问题

--- dictionary.py
+++ dictionary.py
@@ -12,7 +12,10 @@ def getYoudao(word):
         raise Exception('Please input a word ,it must  a str.')
     body = http.request('GET',url)
     soup = BeautifulSoup(body,'html.parser')
+    print(body.status)
+    print(str(soup))
     div = soup.find_all(id = 'phrsListTab')
+    print(div)
     if len(div) != 0:
         return re.findall(r'^<li>(.+?)</li>$',str(div[0]),re.M)
     else:
