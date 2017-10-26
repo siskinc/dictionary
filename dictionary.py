de2d3bd2a3919eb6547ef4e4428e8a250baa1e70
susecjh <susecjh@gmail.com>
2017-10-27 01:22:22 +0800

    修复不能汉译英的bug

--- dictionary.py
+++ dictionary.py
@@ -1,18 +1,39 @@
 from bs4 import BeautifulSoup
+from urllib.parse import quote
 import urllib3
 import re
-import os
 
 http = urllib3.PoolManager()
 
+def check_contain_chinese(check_str):
+    return all('\u4e00' <= char <= '\u9fff' for char in check_str)
+
+def getListOfEnglish(p):
+    for pp in p:
+        a = re.findall(r'<span style="font-weight: bold; color: #959595; margin-right: .5em; width : 36px; display: inline-block;">(.*?)</span>',str(pp),re.M)
+        b = re.findall(r'<a class="search-js" .*>(.*?)</a>',str(pp),re.M)
+        if len(a) != 0 and len(b) != 0:
+            yield [str(a[0])]+[str(c) for c in b]
+
+def chinese_option(soup):
+    p = soup.find_all('p',attrs={'class':'wordGroup'})
+    rets = getListOfEnglish(p)
+    for ret in rets:
+        ret[0] = ret[0]+'  '+ret[1]
+        del ret[1]
+        yield ret
+
+
 def getYoudao(word):
     if isinstance(word,str):
-        url =  'http://dict.youdao.com/w/'+word+'/#keyfrom=dict2.top'
+        url =  'http://dict.youdao.com/w/'+quote(word)+'/#keyfrom=dict2.top'
     else:
         raise Exception('Please input a word ,it must a str.')
     body = http.request('GET',url)
     soup = BeautifulSoup(body.data.decode('utf-8'),'html.parser')
     if body.status == 200:
+        if check_contain_chinese(word):
+            return chinese_option(soup)
         div = soup.find_all(id = 'phrsListTab')
         if len(div) != 0:
             return re.findall(r'^<li>(.+?)</li>$',str(div[0]),re.M)
@@ -28,15 +49,27 @@ if __name__ == '__main__':
         word = input('>>>')
         word = word.replace(' ','%20')
         meanings = getYoudao(word)
-        for meaning in meanings:
-            meaning = meaning.split('；')
+        if not check_contain_chinese(word):
+            for meaning in meanings:
+                meaning = meaning.split('；')
+                count = 0
+                space = (len(meaning[0].split('.')[0])+2) * ' '
+                for m in meaning:
+                    if count == 0:
+                        print(m)
+                    else:
+                        m = space + m
+                        print(m)
+                    count += 1
+        else:
             count = 0
-            space = (len(meaning[0].split('.')[0])+2) * ' '
-            for m in meaning:
-                if count == 0:
-                    print(m)
-                else:
-                    m = space + m
-                    print(m)
-                count += 1
+            for meaning in meanings:
+                space = (len(meaning[0].split('.')[0])+2) * ' '
+                for m in meaning:
+                    if count == 0:
+                        print(m)
+                    else:
+                        m = space + m
+                        print(m)
+                    count += 1
 
--- spiderOfYoudao.py
+++ spiderOfYoudao.py
@@ -2,11 +2,31 @@ from bs4 import BeautifulSoup
 import urllib3
 from re import match,findall,M
 
-http = urllib3.PoolManager()
+def a():
+    http = urllib3.PoolManager()
 
-ret = http.request('GET','http://dict.youdao.com/w/moon/#keyfrom=dict2.top')
-print(ret.status)
-soup = BeautifulSoup(ret.data.decode('utf-8'),'html.parser')
-a = soup.find_all(id='phrsListTab')
-a2 = str(a[0])
-print(findall(r'^<li>(.+?)</li>$',a2,M))
+    ret = http.request('GET','http://dict.youdao.com/w/%E5%BC%80%E5%BF%83/#keyfrom=dict2.top')  
+    print(ret.status)
+    soup = BeautifulSoup(ret.data.decode('utf-8'),'html.parser')
+# a = soup.find_all('a',attrs={'class':'search-js'})
+# print(a)
+# print(a[0].parent.previous_sibling)
+# a = soup.find_all('span')
+# print(a)
+# print(a[0].parent.previous_sibling)
+# for aa in a:
+#     print(aa.span.string+aa.a.string)
+
+    p = soup.find_all('p',attrs={'class':'wordGroup'})
+    for pp in p:
+        a = findall(r'<span style="font-weight: bold; color: #959595; margin-right: .5em; width : 36px; display: inline-block;">(.*?)</span>',str(pp),M)
+        b = findall(r'<a class="search-js" .*>(.*?)</a>',str(pp),M)
+        if len(a) != 0 and len(b) != 0:
+            yield [str(a[0])]+[str(c) for c in b]
+
+
+c = a()
+for cc in c:
+    cc[0] = cc[0]+'  '+cc[1]
+    del cc[1]
+    print(cc)
