97bff8cf2bfc1cdc40c27d559c9aec00765509ba
susecjh <susecjh@gmail.com>
2017-10-26 23:32:00 +0800

    complete

--- dictionary.py
+++ dictionary.py
@@ -9,21 +9,33 @@ def getYoudao(word):
     if isinstance(word,str):
         url =  'http://dict.youdao.com/w/'+word+'/#keyfrom=dict2.top'
     else:
-        raise Exception('Please input a word ,it must  a str.')
+        raise Exception('Please input a word ,it must a str.')
     body = http.request('GET',url)
-    soup = BeautifulSoup(body,'html.parser')
-    print(body.status)
-    print(str(soup))
-    div = soup.find_all(id = 'phrsListTab')
-    print(div)
-    if len(div) != 0:
-        return re.findall(r'^<li>(.+?)</li>$',str(div[0]),re.M)
+    soup = BeautifulSoup(body.data.decode('utf-8'),'html.parser')
+    if body.status == 200:
+        div = soup.find_all(id = 'phrsListTab')
+        if len(div) != 0:
+            return re.findall(r'^<li>(.+?)</li>$',str(div[0]),re.M)
+        else:
+            return None
     else:
+        print('网络不通')
         return None
-
 
 
 if __name__ == '__main__':
     while True:
         word = input('>>>')
-        print(getYoudao(word))
+        meanings = getYoudao(word)
+        for meaning in meanings:
+            meaning = meaning.split('；')
+            count = 0
+            space = (len(meaning[0].split('.')[0])+2) * ' '
+            for m in meaning:
+                if count == 0:
+                    print(m)
+                else:
+                    m = space + m
+                    print(m)
+                count += 1
+
